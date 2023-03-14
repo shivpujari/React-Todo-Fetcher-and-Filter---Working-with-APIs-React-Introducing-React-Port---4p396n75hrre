f40050a8ef83babcdcd74563c035cce1f5228654
shiv pujari <shivanipujari03@gmail.com>
2023-03-14 10:13:21 +0530

    14-03

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { Todo } from './Todo';
+
+
 const App = () => {
+ 
+    const [todo, setTodo] = useState([]);
+    const [loader, setLoader] = useState(true);
+    const [completed, setCompleted] = useState(true);
+    const[notcomplete, setNotcompleted] = useState(true);
+
+
+
+    useEffect(() => {
+     showTodo();
+    }, [])
+    
+
+
+    const showTodo=()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response=> response.json())
+        .then(data=>{
+            setTodo(data.slice(0,20));
+            setLoader(false);
+        });
+    }
+
+    return (
+        <div>
+        <div id="filter-holder">
+        <div>
+        <input onChange={(e)=>{setCompleted(e.target.checked)}} type="checkbox" checked={completed} id="completed-checkbox"/>Completed
+        </div>
+
+        <div>
+        <input onChange={(e) => { setNotcompleted(e.target.checked) }} type="checkbox" checked={notcomplete} id='incompleted-checkbox' /> Incompleted
+    </div>
+
+        </div>
+
+        {loader ? <Loader/> : todo.map((e)=>{
+            if(completed===e.completed || notcomplete !== e.completed){
+                return <Todo key={e.id} id={e.id} title={e.title} completed={e.completed}/>
+            }
+        
+        })
+    }
+    
+    </div>
+    )
   
   
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,22 @@
- import React from 'react';
+import React from 'react';
 
- export const Todo  = () =>{
-    
- }
+export const Todo = (e) => {
+    return (
+        <div>
+            <ul>
+                <li>
+                <div className='todo' id={`todo-${e.id}`}>
+                <div className='todo-title'>
+                {e.title}
+                </div>
+                <div className='todo-status'>
+                {e.completed ? "Completed" : "Incompleted"}
+                </div>
+                </div>
+                </li>
+
+            </ul>
+        </div>
+    )
+
+}
